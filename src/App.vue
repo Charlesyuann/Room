500be101f2ed53888bf23599a3afd238e86c689a
MSI\USER <wyuan0413@gmail.com>
2023-04-30 20:25:05 +0800

    777

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/about">About123</router-link> |
+      <router-link to="/about">if</router-link> |
       <router-link to="/sam">Sam</router-link>
     </nav>
     <router-view/>
